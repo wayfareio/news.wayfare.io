89a2e2784c495183d21b1d13a84d0c2bb9223387
Brandon Arbini <b@arbini.com>
2014-10-06 22:21:54 -0700

    Config in app.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Lobsters
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Requiring our app-specific config
+    require Rails.root.join('extras', 'config')
+
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/extras)
 
